aca231344eb41980b2c524a365642e91d684adbc
iKritixal <97557382+iKritixal@users.noreply.github.com>
2022-05-02 09:58:55 -0400

    Update script6.js

--- script6.js
+++ script6.js
@@ -2,7 +2,7 @@ function colorAnswer() {
    var password = document.getElementById("passwordBox2");
   /*add .toUpperCase() String function so that even if the user types in the password in lower-case, it will check against uppercase password text*/
    var passwordText = password.value.toUpperCase();
-   if(passwordText == "bimini") {
+   if(passwordText == "Bimini") {
     return true;
    }
    alert("ERROR 404!");
